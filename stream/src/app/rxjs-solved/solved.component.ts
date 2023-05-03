97effad180f27a49561f84bf818bdbd3e597a7e5
Anton Gorelov <ant7016@yandex.ru>
2023-05-04 02:53:39 +0300

    feat: improve rxjs solution, add logger

--- stream/src/app/rxjs-solved/solved.component.ts
+++ stream/src/app/rxjs-solved/solved.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { Observable, tap, map, switchMap } from 'rxjs';
 import { FormControl } from '@angular/forms';
 
-export function sleep(ms: number, data?: string) {
-  const start = performance.now();
+import { Observable, tap, map, switchMap } from 'rxjs';
 
-  while (performance.now() - start < ms) {
-    // console.log(data, Math.floor(performance.now() - start));
-  }
-}
+import { Logger } from '../logger.service';
+import { sleep } from '../utils/sleep';
 
 @Component({
   selector: 'app-solved',
@@ -18,25 +13,29 @@ export function sleep(ms: number, data?: string) {
 })
 export class SolvedComponent {
 
+  public logs$ = this._logger.messages$();
+
   public control = new FormControl('');
   public todos$ = this.control.valueChanges
     .pipe(
-      tap((value: string | null) => console.log(value)),
+      tap((value: string | null) => this._logger.log(value)),
       // map(() => []),
-      switchMap((value: string | null) => this._longRequest$(value))
+      switchMap((value: string | null) => this._longRequest$())
     );
 
   constructor(
     private readonly _http: HttpClient,
+    private readonly _logger: Logger,
   ) {}
 
-  private _longRequest$(value: string | null): Observable<any> {
+  private _longRequest$(): Observable<any> {
     return this._http.get('https://jsonplaceholder.typicode.com/todos')
       .pipe(
-        tap((items: any) => sleep(1000)),
+        tap((items: any) => sleep(1000, this._logger)),
         map((todos: any[]) =>
-          todos.map(() => Math.floor(Math.random() * 100).toString())
-        )
+          todos.map(() => Math.floor(Math.random() * 100).toString()).slice(0, 15)
+        ),
+        tap((items: any) => this._logger.log('END long request')),
       );
   }
 
